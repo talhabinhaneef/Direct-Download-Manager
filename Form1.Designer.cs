ef66a0bf2b58b202588643b8d7ba21ae74f10eb6
Mohammad Talha Bin Haneef <71895270+talhahaneef@users.noreply.github.com>
2021-11-24 02:27:32 +0500

    update

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Direct_Download_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblURL = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblURL.Location = new System.Drawing.Point(29, 33);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(42, 16);
+            this.lblURL.TabIndex = 0;
+            this.lblURL.Text = "URL:";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(507, 31);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "&Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(71, 31);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(430, 20);
+            this.txtURL.TabIndex = 2;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(71, 57);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(430, 23);
+            this.progressBar.TabIndex = 3;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(242, 92);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(120, 16);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Downloaded 0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 178);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.lblURL);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Direct Download Manager By Mohammad Talha Bin Haneef";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Direct_Download_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WebClient client;
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string url = txtURL.Text;
+            if (!string.IsNullOrEmpty(url))
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Uri uri = new Uri(url);
+                    string fileName = System.IO.Path.GetFileName(uri.AbsolutePath);
+                    client.DownloadDataAsync(uri, Application.StartupPath+"/"+fileName);
+                } );
+                thread.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new WebClient();
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+            client.DownloadFileCompleted += Client_DownloadFileCompleted;
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+           {
+               progressBar.Minimum = 0;
+               double receive = double.Parse(e.BytesReceived.ToString());
+               double total = double.Parse(e.TotalBytesToReceive.ToString());
+               double percentage = receive / total * 100;
+               lblStatus.Text = $"Downloaded {string.Format("{0:0.##}",percentage)}%";
+               progressBar.Value = int.Parse(Math.Truncate(percentage).ToString());
+
+
+
+           }
+                ));
+        }
+
+        private void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show(" Download Completed !", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
